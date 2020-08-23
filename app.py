f8dc5df692534939f8c4e5faad38a2322d567466
tsunochan0731 <tsunochan0731@gmail.com>
2020-08-23 19:13:33 +0900

    “共有”

--- app.py
+++ app.py
@@ -103,5 +103,5 @@ def calculation():
     return render_template('result.html',result=result)
 
 #おまじない
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
